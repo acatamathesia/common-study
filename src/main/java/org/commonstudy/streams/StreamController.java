03e370d408731ebdb66a0b9fabba2645d3ddff3c
acatamathesia <nkd2019@163.com>
2024-03-22 22:29:03 +0800

    增加stream流测试demo

---
+++ src/main/java/org/commonstudy/streams/StreamController.java
@@ -0,0 +1,17 @@
+package org.commonstudy.streams;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 铠甲勇士
+ * @Description:
+ * @Date: create in 2024/3/22 22:28
+ */
+@RestController
+@RequestMapping("/stream")
+public class StreamController {
+
+
+
+}
